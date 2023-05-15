d6f52c748a037eff4b0c13d429fd9db64dea0879
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2023-05-15 16:00:24 -0600

    fixing data types

--- monohilo_uses_rust/src/main.rs
+++ monohilo_uses_rust/src/main.rs
@@ -10,9 +10,9 @@ fn main() {
     println!("Enter the number of points to use for the approximation of pi: ");
     io::stdin().read_line(&mut input).unwrap();
     // Convert the input to an integer
-    let total_points: i32 = input.trim().parse().unwrap();
+    let total_points: i64 = input.trim().parse().unwrap();
     // Initialize the counter for the number of points inside the circle
-    let mut inside_circle: i32 = 0;
+    let mut inside_circle: i64 = 0;
     // Create a random number generator
     let mut rng = rand::thread_rng();
     // Iterate from 0 to total_points
@@ -24,7 +24,7 @@ fn main() {
         let y: f64 = rng.gen_range(0.0..1.0);
 
         // Calculate the squared Euclidean distance from the origin (0, 0) to the point (x, y)
-        let distance = x.powi(2) + y.powi(2);
+        let distance: f64 = x.powi(2) + y.powi(2);
 
         // Check if the distance is less than or equal to 1 (meaning the point is inside the circle)
         if distance <= 1.0 {
@@ -33,7 +33,7 @@ fn main() {
         }
     }
     // Calculate the approximation of pi using the ratio of points inside the circle to total points, multiplied by 4
-    let pi_aproximation = 4.0 * (inside_circle as f64 / total_points as f64);
+    let pi_aproximation: f64 = 4.0 * (inside_circle as f64 / total_points as f64);
     // Print the approximation of pi
     println!("pi approximation: {}", pi_aproximation);
 }
